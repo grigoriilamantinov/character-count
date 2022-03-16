103679af2eb8d312900f14e9e268dbe86a7e099a
Grigorii Lamantinov <grigoriiborodin@yandex.ru>
2022-03-17 00:42:31 +0300

    add string to char converter

---
+++ src/main/java/App.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+public class App {
+    public static void main(String[] args) {
+        String source = "O hi dogi!";
+        char[] arraySource = source.toCharArray();
+        System.out.println(Arrays.toString(arraySource));
+
+
+    }
+}
