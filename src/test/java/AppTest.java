ec54259fff7114286644e5b477b32fa29862eecf
Grigorii Lamantinov <grigoriiborodin@yandex.ru>
2022-03-19 00:32:09 +0300

    add test

---
+++ src/test/java/AppTest.java
@@ -0,0 +1,21 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppTest {
+
+    @Test
+    void shouldSumTwoDigit() {
+        CounterCharText counter = new CounterCharText();
+        String str = "B!!";
+        Map<Character, Integer> expectedResult = new HashMap<>();
+        expectedResult.put((char) 66, 1);
+        expectedResult.put((char) 33, 2);
+        Map<Character, Integer> actualResult = counter.counterCharacters(str);
+
+        assertEquals(expectedResult, actualResult);
+    }
+}
