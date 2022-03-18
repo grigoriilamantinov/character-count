3b7b81870e311edb5739d25e2a5d8bde957f5e12
Grigorii Lamantinov <grigoriiborodin@yandex.ru>
2022-03-18 23:46:55 +0300

    add all feature, before normal sout

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,7 +1,29 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.Set;
+
 public class App {
     public static void main(String[] args) {
-        String inputText = "G!G B!BB Z!Z !!";
         CounterCharText stats = new CounterCharText();
-        stats.counterCharacters(inputText);
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Добро пожаловать в счётчик символов!\nВведите свою строку: ");
+        String inputText = sc.nextLine();
+
+        boolean isDone = false;
+        Map<String, Map <Character, Integer>> cash = new HashMap<>();
+
+        while (!isDone) {
+            if (cash.containsKey(inputText)) {
+                System.out.println("))))" + cash.get(inputText));
+            } else {
+                System.out.println("Ваш результат:\n" + stats.counterCharacters(inputText));
+            }
+            cash.put(inputText, stats.counterCharacters(inputText));
+            System.out.println("Если желаете закончить, напишите done: ");
+            inputText = sc.nextLine();
+            isDone = inputText.equals("done");
+
+        }
     }
 }
--- src/main/java/CounterCharText.java
+++ src/main/java/CounterCharText.java
@@ -1,10 +1,7 @@
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class CounterCharText {
-    public void counterCharacters(String inputText) {
+    public Map<Character, Integer> counterCharacters(String inputText) {
         char[] inputCharacters = inputText.toCharArray();
 
         Set<Character> uniqueCharSet = new HashSet<>();
@@ -23,39 +20,6 @@ public class CounterCharText {
             countedLetters.put(uniqueletter, counterOfUniqueCases);
             counterOfUniqueCases = 0;
         }
-        System.out.println(countedLetters);
+        return countedLetters;
     }
-//    public void countWithArrayOnly(char[] charSourceArray) {
-//        int uniqueArrayIndex = 0;
-//        char[] uniqueArray = new char[charSourceArray.length];
-//        for (char element : charSourceArray) {
-//
-//            int meetingCounter = 0;
-//            int uniqueMark = 0;
-//
-//            uniqueArray[uniqueArrayIndex] = element;
-//
-//            for (int i = 0; i < charSourceArray.length; i++) {
-//                if (element == charSourceArray[i]) {
-//                    meetingCounter++;
-//                }
-//            }
-//
-//            for (int i = 0; i < uniqueArrayIndex + 1; i++) {
-//                if (element == uniqueArray[i]) {
-//                    uniqueMark++;
-//                }
-//            }
-//
-//            if (uniqueMark == 1) {
-//                System.out.println(charSourceArray[uniqueArrayIndex] + " : " + meetingCounter);
-//            }
-//            uniqueArrayIndex++;
-//        }
-//    }
-
-
-//        for (Map.Entry<Character, Integer> entry : map.entrySet()) {
-//            System.out.println(entry.getKey() + " : " + entry.getValue());
-//        }
 }
